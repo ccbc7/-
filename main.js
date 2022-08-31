dac5546ad0642418fa4d96d0cf069d1ffd4ee6fd
ccbc7 <flexibleelbow@gmail.com>
2022-08-31 16:46:09 +0900

    １回目の提出です。

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+const result = document.getElementById("result");
+function edit(value) {
+  result.value = result.value + value;
+}
+
+function calc() {
+  result.value = new Function(
+    "return " + result.value
+  )();
+}
+
+function reset() {
+  result.value = "";
+}
